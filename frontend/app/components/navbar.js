f92c0ad9f200c303588a6930e3d144ab932f6beb
DurevshChaudhari <durveshchaudhari11@gmail.com>
2024-03-14 23:29:12 +0530

    navbar done

--- frontend/app/components/navbar.js
+++ frontend/app/components/navbar.js
@@ -7,11 +7,11 @@ import { Disclosure } from "@headlessui/react";
 
 const Navbar = () => {
   const navigation = [
-    "Product",
-    "Features",
-    "Pricing",
-    "Company",
-    "Blog",
+    "Home",
+    "Complaints",
+    "Water",
+    "Electricity",
+    "About Us",
   ];
 
   return (
@@ -25,15 +25,9 @@ const Navbar = () => {
                 <Link href="/">
                   <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
                     <span>
-                      <Image
-                        src="/img/logo.svg"
-                        alt="N"
-                        width="32"
-                        height="32"
-                        className="w-8"
-                      />
+                     
                     </span>
-                    <span>Nextly</span>
+                    <span>JanhitConnect</span>
                   </span>
                 </Link>
 
