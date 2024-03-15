68dc71b62107f0678b2523bbc6b63a0e7c6e907d
heramb_45 <heramb.bhoodhar@gmail.com>
2024-03-15 11:20:45 +0530

    ...

--- frontend/app/components/hero.js
+++ frontend/app/components/hero.js
@@ -10,16 +10,43 @@ import home from "../public/img/Ministry/home.png";
 import railway from "../public/img/Ministry/railway.png";
 import water from "../public/img/Ministry/water.png";
 import road from "../public/img/Ministry/road.png";
+import { useEffect, useState } from "react";
+// import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const Hero = () => {
+  const router = useRouter();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState();
+  useEffect(() => {
+    const uname = localStorage.getItem("username");
+    console.log(uname);
+    if (uname !== undefined) {
+      setUsername(() => uname);
+      setIsLoggedIn(false);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+
+  function handleRegisterComplaint() {
+    const username = localStorage.getItem("username");
+
+    console.log(isLoggedIn, username, "adashgdjgasjdgjasgdj");
+    if (isLoggedIn) {
+      router.push("/raise-complaint");
+    } else {
+      router.push("/login");
+    }
+  }
   return (
     <>
       <Container className="flex flex-wrap ">
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-            हर समस्या का हल, जनहित कनेक्ट के साथ आओ, मिलकर बदले ज़िंदगी!
-
+              हर समस्या का हल, जनहित कनेक्ट के साथ आओ, मिलकर बदले ज़िंदगी!
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
               Janhit helps to connect the peoples problems with the responsible
@@ -27,14 +54,12 @@ const Hero = () => {
             </p>
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
-              <a
-                href="https://web3templates.com/templates/nextly-landing-page-template-for-startups"
-                target="_blank"
-                rel="noopener"
+              <button
+                onClick={handleRegisterComplaint}
                 className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md "
               >
                 Register complaint
-              </a>
+              </button>
             </div>
           </div>
         </div>
