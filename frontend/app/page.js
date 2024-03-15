41ac1121dfe7eadf1934c62f70f44dce02b2797f
bhaveshxop <113847606+bhaveshxop@users.noreply.github.com>
2024-03-15 07:19:28 +0530

    ui changes

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -16,10 +16,10 @@ const page = () => {
   return (
     <>
       <Head>
-        <title>Nextly - Free Nextjs & TailwindCSS Landing Page Template</title>
+        <title>Janhit - Adressing the peoples</title>
         <meta
           name="description"
-          content="Nextly is a free landing page template built with next.js & Tailwind CSS"
+          content="Nextly is a free lanith next.js & Tailwind CSS"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -27,17 +27,17 @@ const page = () => {
       <Navbar />
       <Hero />
       <SectionTitle
-        pretitle="Nextly Benefits"
-        title=" Why should you use this landing page">
-        Nextly is a free landing page & marketing website template for startups
-        and indie projects. Its built with Next.js & TailwindCSS. And its
-        completely open-source.
+        pretitle="जनहित  Benefits"
+        title=" Why should you use जनहित connect?"
+      >
+        To get your problem in front of the right people, you need to highlight
       </SectionTitle>
       <Benefits data={benefitOne} />
       <Benefits imgPos="right" data={benefitTwo} />
       <SectionTitle
         pretitle="Watch a video"
-        title="Learn how to fullfil your needs">
+        title="Learn how to fullfil your needs"
+      >
         This section is to highlight a promo or demo video of your product.
         Analysts says a landing page with video has 3% more conversion rate. So,
         don&apos;t forget to add one. Just like this.
@@ -45,7 +45,8 @@ const page = () => {
       <Video />
       <SectionTitle
         pretitle="Testimonials"
-        title="Here's what our customers said">
+        title="Here's what our customers said"
+      >
         Testimonails is a great way to increase the brand trust and awareness.
         Use this section to highlight your popular customers.
       </SectionTitle>
@@ -57,9 +58,9 @@ const page = () => {
       <Faq />
       <Cta />
       <Footer />
-      <PopupWidget />   
+      <PopupWidget />
     </>
   );
-}
+};
 
 export default page;
