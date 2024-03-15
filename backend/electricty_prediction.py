326ec21937d6ad2652a385fd45cf245148fb6688
DurevshChaudhari <durveshchaudhari11@gmail.com>
2024-03-16 01:25:19 +0530

    electricity dataset done

--- backend/electricty_prediction.py
+++ backend/electricty_prediction.py
@@ -3,7 +3,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
 # Load numerical data into a DataFrame
-data = pd.read_csv('data.csv')
+data = pd.read_csv('dataset_electricity.csv')
 
 # Select only specific columns as features
 selected_features = ['People in Ward', 'Festive Season', 'Construction']  # List of column names you want to include
@@ -19,18 +19,18 @@ rf_regressor.fit(X, y)
 # Take input from the user
 ward_no = int(input("Enter Ward No: "))
 people_in_ward = int(input("Enter People in Ward: "))
-season = input("Enter Season (Spring/Summer/Fall): ")
+season = input("Is this Festive Season (Yes/NO): ")
 construction = input("Enter Construction (True/False): ")
 
 # Make predictions using user input
 input_data = pd.DataFrame({
     'People in Ward': [people_in_ward],
-    'Season': [season],
+    'Festive_Season': [season],
     'Construction': [construction]
 })
 
 # Encode categorical variables
-input_data['Season'] = input_data['Season'].map({'Spring': 0, 'Summer': 1, 'Fall': 2})
+input_data['Festive_Season'] = input_data['Festive_Season'].map({'Yes': 1, 'No': 0})
 input_data['Construction'] = input_data['Construction'].map({'True': 1, 'False': 0})
 
 # Predict water utilization
